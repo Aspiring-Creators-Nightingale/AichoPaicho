1c7dec102c4a7ce8da06dcbffa6e8440a3e8c23c
Apil Khadka <apilkhadka2020@gmail.com>
2025-09-16 11:51:23 +0545

    welcome screen ui finished

---
+++ app/src/main/java/com/aspiring_creators/aichopaicho/ui/component/AppComponent.kt
@@ -0,0 +1,139 @@
+package com.aspiring_creators.aichopaicho.ui.component
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.aspiring_creators.aichopaicho.R
+import java.nio.file.WatchEvent
+import kotlin.math.log
+
+
+@Composable
+fun LogoTopBar(logo: Int, title: String)
+{
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Spacer(modifier = Modifier.size(36.dp))
+        Icon(
+            painter = painterResource(id = logo)
+            , contentDescription = "Title",
+            tint = Color.Unspecified
+        )
+        Spacer(modifier = Modifier.size(36.dp))
+
+       TextComponent("AichoPaicho", textSize = 35.sp)
+
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun LogoTopBarPreview()
+{
+    LogoTopBar(logo = R.drawable.logo_aichopaicho, title = "Aicho Paicho")
+}
+
+@Composable
+fun TextComponent(value: String, textSize: TextUnit = 12.sp) {
+
+
+    val crimsonTextFamily = FontFamily(
+        Font(R.font.crimson_regular, FontWeight.Normal),
+         Font(R.font.crimson_bold, FontWeight.Bold),
+         Font(R.font.crimson_italic, FontWeight.Normal, FontStyle.Italic)
+    )
+
+    Text(
+        text = value,
+        modifier = Modifier.padding(10.dp),
+        fontFamily = crimsonTextFamily,
+        color = Color.Black,
+        fontSize = textSize,
+        fontWeight = FontWeight.Normal,
+    )
+}
+@Preview(showBackground = true)
+@Composable
+fun TextComponentPreview()
+{
+    TextComponent(value = "Welcome to Aicho Paicho", textSize = 27.sp)
+}
+
+
+@Composable
+fun ButtonComponent(
+    logo: Int,
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp),
+         colors = ButtonDefaults.buttonColors(
+             containerColor = colorResource(R.color.buttonColor),
+             contentColor = colorResource(R.color.black)
+         ),
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 12.dp) // Control padding inside the button
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(logo),
+                contentDescription = "$text logo",
+                tint = Color.Unspecified,
+
+                modifier = Modifier.size(28.dp)
+            )
+            Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+
+            TextComponent(
+                value = text,
+                textSize = 20.sp,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ButtonComponentPreview()
+{
+    ButtonComponent(
+        logo = R.drawable.logo_google, text = "Sign in with Google",
+        onClick = {},
+        modifier = Modifier
+    )
+}
+
+
+
+
+
--- app/src/main/java/com/aspiring_creators/aichopaicho/ui/screens/WelcomeScreen.kt
+++ app/src/main/java/com/aspiring_creators/aichopaicho/ui/screens/WelcomeScreen.kt
@@ -1,38 +1,75 @@
 package com.aspiring_creators.aichopaicho.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.aspiring_creators.aichopaicho.R
+import com.aspiring_creators.aichopaicho.ui.component.ButtonComponent
+import com.aspiring_creators.aichopaicho.ui.component.LogoTopBar
+import com.aspiring_creators.aichopaicho.ui.component.TextComponent
 
 
 @Composable
 fun WelcomeScreen()
 {
     Surface(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxWidth(),
+        color = colorResource(R.color.appThemeColor)
     ) {
         Column(
             modifier = Modifier.fillMaxWidth()
         ) {
 
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
+            LogoTopBar(logo = R.drawable.logo_aichopaicho, title = "AichoPaicho")
 
+            Spacer(modifier = Modifier.size(33.dp))
 
+            Image(
+                painter = painterResource(id = R.drawable.welcome_screen_1),
+                contentDescription = "welcome screen",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .padding(horizontal = 22.dp)
+                    .clip(CircleShape)
+            )
 
-            }
+            Spacer(modifier = Modifier.size(33.dp))
 
+            TextComponent(value = "Never forget a loan or a debt", textSize = 37.sp)
+
+            Spacer(modifier = Modifier.size(33.dp))
+
+            ButtonComponent(
+                R.drawable.logo_google, "Sign in with Google",
+                onClick = {},
+                modifier = Modifier
+            )
+
+            ButtonComponent(
+                R.drawable.logo_skip, "Skip for Now",
+                onClick = {},
+                modifier = Modifier.padding(horizontal = 85.dp).width(200.dp)
+            )
         }
-
-
-
     }
-
 }
 
 @Preview(showBackground = true)
